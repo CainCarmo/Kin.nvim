466c52a4862f5a555bff94325a633eb02ee3d5fb
caincarmo <caina.developer@gmail.com>
2025-02-25 16:01:42 -0300

    refactor(uninstaller): Add backup option for 'knvim.lua' before removal

--- utils/scripts/uninstaller.ps1
+++ utils/scripts/uninstaller.ps1
@@ -1,7 +1,38 @@
-$paths = @("$env:USERPROFILE/AppData/Local/nvim", "$env:USERPROFILE/AppData/Local/nvim-data")
+$knvim_paths = @("$env:LOCALAPPDATA/nvim", "$env:LOCALAPPDATA/nvim-data")
 
-foreach ($path in $paths) {
-  if (Test-Path -Path $path) {
-    Remove-Item -Path $path -Recurse -Force
+function BackupKnvim {
+  Write-Host " 🍡 Kin.nvim Uninstaller "
+  Write-Host "─────────────────────────"
+  Write-Host ""
+
+  $confirm = Read-Host "Do you want to backup your 'knvim.lua' configuration? [y/N] "
+
+  $knvim_file_path = "$env:LOCALAPPDATA/nvim/lua/user/knvim.lua"
+  $knvim_backup_path = "$env:LOCALAPPDATA/nvim.bak/"
+
+  if (Test-Path -Path "$knvim_file_path" && $confirm -match "^[yY]$") {
+    New-Item -Path "$knvim_backup_path" -ItemType Directory -Force
+    Copy-Item -Path "$knvim_file_path" -Destination "$knvim_backup_path" -Force
+
+    Write-Host "✔ Backup created: $knvim_backup_path"
+  }
+  else {
+    Write-Host (Test-Path -Path "$knvim_file_path") ? "⚠ File 'knvim.lua' not found." : "⚠ Backup skipped..."
   }
 }
+
+function Uninstaller {
+  BackupKnvim
+
+  foreach ($path in $knvim_paths) {
+    if (Test-Path -Path $path) {
+      Remove-Item -Path $path -Recurse -Force
+      Write-Host "✔ Removed: $path"
+    }
+    else {
+      Write-Host "⚠ Path not found: $path"
+    }
+  }
+}
+
+Uninstaller
--- utils/scripts/uninstaller.sh
+++ utils/scripts/uninstaller.sh
@@ -1,17 +1,51 @@
 #!/usr/bin/env bash
 
-if [ -d "$HOME/.config/nvim" ]; then
-  rm -rf "$HOME/.config/nvim"
-fi
+backup_knvim() {
+  echo " 🍡 Kin.nvim Uninstaller "
+  echo "─────────────────────────"
+  echo ""
 
-if [ -d "$HOME/.local/share/nvim" ]; then
-  rm -rf "$HOME/.local/share/nvim"
-fi
+  read -n 1 -r -p "Do you want to backup your 'knvim.lua' configuration? [y/N] " confirm_knvim
+  echo ""
 
-if [ -d "$HOME/.local/state/nvim" ]; then
-  rm -rf "$HOME/.local/state/nvim"
-fi
+  knvim_file_path="$HOME/.config/nvim/lua/user/knvim.lua"
+  knvim_backup_path="$HOME/.config/nvim.bak/"
 
-if [ -d "$HOME/.cache/nvim" ]; then
-  rm -rf "$HOME/.cache/nvim"
-fi
+  if [[ -f "$knvim_file_path" && "$confirm_knvim" =~ ^[Yy]$ ]]; then
+    mkdir -p "$knvim_backup_path"
+
+    if mv "$knvim_file_path" "$knvim_backup_path/"; then
+      echo "✔ Backup successful. File saved at '$knvim_backup_path'."
+    else
+      echo "🗙 Backup failed."
+    fi
+  else
+    [[ ! -f "$knvim_file_path" ]] && echo "⚠ File 'knvim.lua' not found." || echo "⚠ Backup skipped..."
+  fi
+}
+
+uninstaller() {
+  backup_knvim
+
+  if [ -d "$HOME/.config/nvim" ]; then
+    rm -rf "$HOME/.config/nvim"
+    echo "✔ Deleted file: $HOME/.config/nvim"
+  fi
+
+  if [ -d "$HOME/.local/share/nvim" ]; then
+    rm -rf "$HOME/.local/share/nvim"
+    echo "✔ Deleted file: $HOME/.local/share/nvim"
+  fi
+
+  if [ -d "$HOME/.local/state/nvim" ]; then
+    rm -rf "$HOME/.local/state/nvim"
+    echo "✔ Deleted file: $HOME/.local/state/nvim"
+  fi
+
+  if [ -d "$HOME/.cache/nvim" ]; then
+    rm -rf "$HOME/.cache/nvim"
+    echo "✔ Deleted file: $HOME/.cache/nvim"
+  fi
+}
+
+uninstaller
