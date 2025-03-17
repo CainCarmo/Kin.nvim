2a29f13e65bbce385dbff6aaa28f4ddaadcbee18
CainCarmo <caina.developer@gmail.com>
2025-03-17 01:25:32 -0300

    feat(types): Add Icons class with field type annotations for UI, Git, Kind, Misc, Chars and Diagnostics

---
+++ lua/user/utils/types/icons.lua
@@ -0,0 +1,9 @@
+---@meta
+
+---@class Icons
+---@field ui table<string, string>
+---@field git table<string, string>
+---@field kind table<string, string>
+---@field misc table<string, string>
+---@field chars table<string, string>
+---@field diagnostics table<string, string>
