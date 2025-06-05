06ded11d5e9ed6e04a9cae37a5f2eca4a3bff6a0
CainCarmo <caina.developer@gmail.com>
2025-06-05 00:19:40 -0300

    chore(keymap): Refactor the `keymap` file by removing unused mappings and adding new ones for specific plugins

--- lua/user/config/keymap.lua
+++ lua/user/config/keymap.lua
@@ -1,36 +1,27 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
-vim.api.nvim_set_keymap("t", "<C-;>", "<C-\\><C-n>", opts)
+-- Built-in
+keymap("n", "<ESC>", "<cmd>noh<CR>", opts) -- #: Clear search highlights
 
-keymap("n", "<ESC>", "<cmd>noh<CR>", opts)
+keymap("v", "<", "<gv", opts) -- #: Indent left
+keymap("v", ">", ">gv", opts) -- #: Indent right
 
-keymap("n", "<m-h>", "<C-w>h", opts)
-keymap("n", "<m-j>", "<C-w>j", opts)
-keymap("n", "<m-k>", "<C-w>k", opts)
-keymap("n", "<m-l>", "<C-w>l", opts)
-keymap("n", "<m-tab>", "<c-6>", opts)
+keymap({ "n", "v" }, ";", ":", opts) -- #: Command mode
+keymap({ "n", "v" }, "<C-z>", "u", opts) -- #: Undo
+keymap({ "n", "v" }, "<C-s>", "<cmd>w<CR>", opts) -- #: Save
+keymap({ "n", "v" }, "<C-/>", "gcc", opts) -- #: Toggle comment line
+keymap({ "n", "v" }, "<A-Up>", "<Esc>:m .-2<CR>", opts) -- #: Move line up
+keymap({ "n", "v" }, "<A-Down>", "<Esc>:m .+1<CR>", opts) -- #: Move line down
 
-keymap("n", "n", "nzz", opts)
-keymap("n", "N", "Nzz", opts)
-keymap("n", "*", "*zz", opts)
-keymap("n", "#", "#zz", opts)
-keymap("n", "g*", "g*zz", opts)
-keymap("n", "g#", "g#zz", opts)
+-- Plugins
+keymap({ "n", "v" }, "<S-o>", function()
+  vim.cmd [[ AerialToggle! ]]
+end, opts) -- #: Aerial (Toggle)
 
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap({ "n", "v" }, "<TAB>", "<cmd>bnext<CR>", opts) -- #: Tabline (Next buffer)
+keymap({ "n", "v" }, "<S-TAB>", "<cmd>bprevious<CR>", opts) -- #: Tabline (Previous buffer)
+keymap({ "n", "v" }, "<S-w>", "<cmd>bdelete<CR>", opts) -- #: Tabline (Delete buffer)
 
-keymap("x", "p", [["_dP]])
-
-keymap({ "n", "o", "x" }, "<s-h>", "^", opts)
-keymap({ "n", "o", "x" }, "<s-l>", "g_", opts)
-keymap({ "n", "v" }, ";", ":", opts)
-keymap({ "n", "v" }, "<C-s>", "<cmd>w<CR>", opts)
-keymap({ "n", "v" }, "<C-y>", "<C-r>", opts)
-keymap({ "n", "v" }, "<C-z>", "u", opts)
-keymap({ "n", "v" }, "<A-Up>", "<Esc>:m .-2<CR>", opts)
-keymap({ "n", "v" }, "<A-Down>", "<Esc>:m .+1<CR>", opts)
-keymap({ "n", "v" }, "<TAB>", "<cmd>bnext<CR>", opts)
-keymap({ "n", "v" }, "<S-TAB>", "<cmd>bprevious<CR>", opts)
-keymap({ "n", "v" }, "<A-w>", "<cmd>bdelete<CR>", opts)
+keymap("t", "<ESC>", "<C-\\><C-n>", opts) -- #: ToggleTerm (Exit terminal mode)
+keymap({ "n", "v", "t" }, "<C-ESC>", "<cmd>ToggleTerm<CR>", opts) -- #: ToggleTerm (Open Terminal)
