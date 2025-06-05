a48b4a4c8111d8bdf02f16af98818afa02f4f9f0
CainCarmo <caina.developer@gmail.com>
2025-06-05 00:10:24 -0300

    chore: Remove unused key mappings from `which-key` configuration

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -5,15 +5,12 @@ return {
     spec = {
       mode = { "n", "v" },
       { "<leader>a", group = "Active Plugins" },
-      { "<leader>b", group = "Winbar" },
       { "<leader>d", group = "Debugger" },
       { "<leader>f", group = "Search" },
       { "<leader>g", group = "Git" },
       { "<leader>l", group = "Lsp" },
-      { "<leader>n", group = "Notes" },
-      { "<leader>N", group = "Noice" },
+      { "<leader>n", group = "Noice" },
       { "<leader>s", group = "Sessions" },
-      { "<leader>w", group = "Web Development" },
     },
     win = {
       no_overlap = true,
