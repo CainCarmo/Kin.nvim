1ae749a3615be464cb12e100d7b69ac1870faa82
CainCarmo <caina.developer@gmail.com>
2026-06-04 22:43:41 -0300

    feat: add `lazy.nvim` package manager configuration.

---
+++ lua/knvim/packager/init.lua
@@ -0,0 +1,77 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local output = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=master",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Error cloning lazy.nvim: " .. output, "ErrorMsg" },
+      { "\nPress any key to continue..." },
+    })
+
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup {
+  spec = {
+    { import = "knvim.plugins" },
+  },
+  ui = {
+    border = "rounded",
+  },
+  install = {
+    colorscheme = {},
+  },
+  checker = {
+    enabled = true,
+    check_pinned = true,
+  },
+  chance_detection = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
+}
