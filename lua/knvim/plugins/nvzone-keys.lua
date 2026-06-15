d85035eeef6f10d5cb91b1968d54b8acea19e306
CainCarmo <caina.developer@gmail.com>
2026-06-15 16:20:43 -0300

    feat(plugins): add `nvzone/showkeys` plugin for displaying typed keys.

---
+++ lua/knvim/plugins/nvzone-keys.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      timeout = 2,
+      maxkeys = 5,
+      position = "top-center",
+    },
+    dependencies = "nvzone/volt",
+  },
+}
