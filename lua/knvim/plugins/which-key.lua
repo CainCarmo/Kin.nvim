6838512200ca093c39a68fba2950bbf5e3af8936
CainCarmo <caina.developer@gmail.com>
2026-06-17 03:08:17 -0300

    chore(which-key): customize plugin UI and behavior.

--- lua/knvim/plugins/which-key.lua
+++ lua/knvim/plugins/which-key.lua
@@ -1,6 +1,18 @@
 return {
   "folke/which-key.nvim",
   keys = {},
-  opts = {},
+  opts = {
+    preset = "modern",
+    plugins = {
+      marks = false,
+      regsiters = false,
+    },
+    win = {
+      padding = { 2, 2 },
+    },
+    disable = {
+      bt = { "TelescopePrompt" },
+    },
+  },
   event = "VeryLazy",
 }
