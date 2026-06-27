aa23c814b4bc0b46e8ae63c19b5926dd36fede2a
CainCarmo <caina.developer@gmail.com>
2026-06-27 01:35:35 -0300

    feat(lspconfig): add initialization function for semantic tokens in LSP config.

--- lua/knvim/plugins/lspconfig.lua
+++ lua/knvim/plugins/lspconfig.lua
@@ -1,6 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {},
+  opts = {
+    on_init = function(client, _)
+      if vim.fn.has "nvim-0.11" ~= 1 then
+        if client.supports_method "textDocument/semanticTokens" then
+          client.server_capabilities.semanticTokensProvider = nil
+        end
+      else
+        if client:supports_method "textDocument/semanticTokens" then
+          client.server_capabilities.semanticTokensProvider = nil
+        end
+      end
+    end,
+  },
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "williamboman/mason.nvim",
